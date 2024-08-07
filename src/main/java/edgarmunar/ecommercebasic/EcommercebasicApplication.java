725be0df5c443e3a15cc4d8d6e03f9038f1fdd6b
Edgar Munar <edgar_munar@hotmail.com>
2024-08-07 11:15:58 -0500

    first commit

---
+++ src/main/java/edgarmunar/ecommercebasic/EcommercebasicApplication.java
@@ -0,0 +1,13 @@
+package edgarmunar.ecommercebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercebasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercebasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edgarmunar/ecommercebasic/EcommercebasicApplicationTests.java
@@ -0,0 +1,13 @@
+package edgarmunar.ecommercebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercebasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
